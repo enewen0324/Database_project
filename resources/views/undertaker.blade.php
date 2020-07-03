df47125916924733984a406c229ff6eca6b9f73f
Bob <bobhsiao.cs07@nctu.edu.tw>
2020-07-03 14:06:24 +0800

    update undertaker.blade.php

--- resources/views/undertaker.blade.php
+++ resources/views/undertaker.blade.php
@@ -1,7 +1,63 @@
 
 
-@foreach($variable as $ele)
-    <div> {{$ele->city_name}} </div>
-    <div> {{$ele->facility_name}} </div>
-    <div> {{$ele->phone}} </div>
-@endforeach
+<style>
+    th{
+        font-family: monospace;
+        line-height: 45px;
+        font-size: 20px;
+    }
+    td{
+        font-family: monospace;
+        line-height: 45px;
+        font-size: 15px;
+        display: flex;
+        align-items: center;
+    }
+    tr:nth-child(even){
+        background-color: lightgrey;
+    }
+    table{
+        display: flex;
+        justify-content: center;
+        text-align: center;
+        background-color: white;
+        border-collapse: collapse;
+    }
+    table, td{
+        border: solid, black;
+    }
+    .undertakerTable{
+        display: flex;
+        justify-content: center;
+    }
+    .undertakerName{
+        display: flex;
+        width: 30%;
+    }
+    .undertakerAddress{
+        display: flex;
+        width: 50%;
+    }
+    .undertakerPhone{
+        display: flex;
+        width: 20%;
+    }
+</style>
+
+<div class="undertakerTable">
+    <table>
+        <tr>
+            <th>名稱</th>
+            <th>聯絡地址</th>
+            <th>聯絡電話</th>
+        </tr>
+        @foreach($variable as $ele)
+        <tr>
+            <td class="undertakerName"> {{$ele->facility_name}}</td>
+            <td class="undertakeAddress"> {{$ele->address}} </td>
+            <td class="undertakerPhone"> {{$ele->phone}}</td>
+        </tr>
+        @endforeach
+    </table>
+</div>
+
