e31527c50bf1e2cfb04dca93dd2bec4151584f62
Bob <bobhsiao.cs07@nctu.edu.tw>
2020-07-03 15:50:49 +0800

    update fireplace and modify undertaker

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -4,53 +4,54 @@
     }
 </style>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <a class="navbar-brand" href="/index">
-                <span class="h3 mx-1"> 人生終點站 </span>
-            </a>
+        <a class="navbar-brand" href="/index">
+            <span class="h3 mx-1"> 人生終點站 </span>
+        </a>
 
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav mr-auto">
-                    <li class="nav-item">
-                        <a class="nav-link" href="/index">首頁<span class="sr-only">(current)</span></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/customizedSearch">  預定 / 查詢</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#"> 最新消息 </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/etiquette/all"> 禮儀社 </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/undertaker/all"> 殯儀館 </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/fireplace/all"> 火化場 </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/pagoda/all"> 墓園寶塔 </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#"> 生前契約 </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/greenburial/all"> 自然葬 </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#"> F A Q </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#"> 聯絡我們 </a>
-                    </li>
-                </ul>
-                <form class="form-inline my-2 my-lg-0">
-                    <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
-                        <button class="btn btn-outline-success my-2 my-sm-0" type="submit"> Search </button>
-                </form>
-            </div>
-        </nav>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="/index">首頁<span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/customizedSearch">  預定 / 查詢</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#"> 最新消息 </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/etiquette/all"> 禮儀社 </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/undertaker/all"> 殯儀館 </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/fireplace/all"> 火化場 </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/pagoda/all"> 墓園寶塔 </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#"> 生前契約 </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/greenburial/all"> 自然葬 </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#"> F A Q </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#"> 聯絡我們 </a>
+                </li>
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit"> Search </button>
+            </form>
+        </div>
+</nav>
+
--- resources/views/undertaker.blade.php
+++ resources/views/undertaker.blade.php
@@ -1,4 +1,18 @@
-
+<html>
+    <head>
+        <link rel="stylesheet" href="https://cdn.staticfile.org/twitter-bootstrap/4.3.1/css/bootstrap.min.css">
+        <script src="https://cdn.staticfile.org/jquery/3.2.1/jquery.min.js"></script>
+        <script src="https://cdn.staticfile.org/popper.js/1.15.0/umd/popper.min.js"></script>
+        <script src="https://cdn.staticfile.org/twitter-bootstrap/4.3.1/js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.min.js"></script>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title> Undertaker </title>
+    </head>
+    <body>
+        @include('includes.navbar')
+    </body>
+</html>
 
 <style>
     th{
@@ -26,24 +40,42 @@
     table, td{
         border: solid, black;
     }
+    .undertakerTitle{
+        display: flex;
+        font-family: monospace;
+        font-size: 25px;
+        justify-content: center;
+        align-items: center;
+        height: 100px;
+    }
+    .titleText{
+        height: 50%;
+        width: 20%;
+        border-bottom: solid 2px;
+        display: flex;
+        justify-content: center;
+    }
     .undertakerTable{
         display: flex;
         justify-content: center;
     }
     .undertakerName{
         display: flex;
-        width: 30%;
+        width: 25%;
     }
     .undertakerAddress{
         display: flex;
-        width: 50%;
+        width: 30%;
     }
     .undertakerPhone{
         display: flex;
-        width: 20%;
+        width: 25%;
     }
 </style>
 
+<div class="undertakerTitle">
+    <div class="titleText">合作殯儀館</div>
+</div>
 <div class="undertakerTable">
     <table>
         <tr>
